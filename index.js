af052680b05e61135f1dbeace3d9365a5e779ed6
Justin Tooley <jstooley823@gmail.com>
2018-04-18 03:51:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function produceDrivingRange(max_distance){
+  return function withinRange (start, finish) {
+
+  let distance  = Math.abs(parseInt(finish) - parseInt(start));
+
+  if(distance > max_distance){
+      return `${distance - max_distance} blocks out of range`;
+    } else{
+      return `within range by ${max_distance - distance}`;
+    }
+  }
+}
+function produceTipCalculator(percent){
+  return function tipPercent(fare){
+    return fare * percent
+  }
+}
