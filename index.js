36668abc3f8f30ea668b40220dc10015a3b14996
Justin Tooley <jstooley823@gmail.com>
2018-04-18 03:46:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 function produceDrivingRange(max_distance){
   return function withinRange (start, finish) {
 
-    start = start.slice(0,2);
-    finsih = finish.slice(0,2);
+  let distance  = Math.ads(parseInt(finish) - parseInt(start));
 
     if(distance > max_distance){
       return false;
@@ -12,5 +11,7 @@ function produceDrivingRange(max_distance){
   }
 }
 function distacneBetween(start,finish){
-
+  start = start.slice(0,2);
+  finsih = finish.slice(0,2);
+    return parseInt(finish) - parseInt(start)
 }
