ea3e0d3be9007c42ea1670acbeb07e2262b8937b
Justin Tooley <jstooley823@gmail.com>
2018-04-18 03:50:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function produceDrivingRange(max_distance){
 
   let distance  = Math.abs(parseInt(finish) - parseInt(start));
 
-    if(distance > max_distance){
+  if(distance > max_distance){
       return `${distance - max_distance} blocks out of range`;
     } else{
       return `within range by ${max_distance - distance}`;
